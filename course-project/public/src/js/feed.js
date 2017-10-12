0e5df206d79ebce5139c30e2ec777b95aab856d5
Hans Maes <Hans.Maes@SolidSyntax.net>
2017-10-12 20:47:01 +0200

    cache strategies, part 1

--- course-project/public/src/js/feed.js
+++ course-project/public/src/js/feed.js
@@ -31,6 +31,18 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+// Not used, example to show how to cache one user demand
+const onSaveButtonClicked = (event) => {
+    console.log('Clicked');
+    if('caches' in window){
+    caches.open('user-requested')
+        .then(cache => {
+            cache.add('https://httpbin.org/get');
+            cache.add('/src/images/sf-boat.jpg');
+        });
+    }
+};
+
 function createCard() {
     const cardWrapper = document.createElement('div');
     cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -48,6 +60,12 @@ function createCard() {
     cardSupportingText.className = 'mdl-card__supporting-text';
     cardSupportingText.textContent = 'In San Francisco';
     cardSupportingText.style.textAlign = 'center';
+
+    // const cardSaveButton = document.createElement('button');
+    // cardSaveButton.textContent = 'Save';
+    // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+    // cardSupportingText.appendChild(cardSaveButton);
+
     cardWrapper.appendChild(cardSupportingText);
     componentHandler.upgradeElement(cardWrapper);
     sharedMomentsArea.appendChild(cardWrapper);
--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,5 +1,6 @@
-const STATIC_CACHE_KEY = 'static-v2';
+const STATIC_CACHE_KEY = 'static';
 const DYNAMIC_CACHE_KEY = 'dynamic';
+
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(STATIC_CACHE_KEY) // Add versioning to force a refresh of the cached elements
@@ -8,6 +9,7 @@ self.addEventListener('install', event => {
                 cache.addAll([
                     '/',
                     '/index.html',
+                    '/offline.html',
                     '/src/js/app.js',
                     '/src/js/feed.js',
                     '/src/js/promise.js',
@@ -35,23 +37,66 @@ self.addEventListener('activate', event => {
         caches.keys()
             .then(keylist => Promise.all( // Take an array of promises and wait for all to finish
                 keylist
-                    .filter(key => key !== STATIC_CACHE_KEY && key !==DYNAMIC_CACHE_KEY)
+                    .filter(key => key !== STATIC_CACHE_KEY && key !== DYNAMIC_CACHE_KEY)
                     .map(key => caches.delete(key))))
     );
     return self.clients.claim();
 });
 
+
+// Cache with network fallback
+// This cache strategy prefers the cache
+// If a resource is found in the cache no fetch request will be made
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(cacheResponse => cacheResponse ? cacheResponse :
+//                 fetch(event.request)
+//                     .then(fetchResponse => caches.open(DYNAMIC_CACHE_KEY).then( // Dynamic cache fetch requests
+//                         cache => {
+//                             cache.put(event.request.url, fetchResponse.clone()); // Clone because put consumes the response
+//                             return fetchResponse
+//                         }))
+//                     .catch(error =>{
+//                         return caches.open(STATIC_CACHE_KEY)
+//                             .then(cache => cache.match('/offline.html'));
+//                     })
+//             )
+//     );
+// });
+
+// This strategy only uses the cache
+// The network will never be used
+// This is not a valid strategy for dynamic content
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//     );
+// });
+
+// This strategy only uses the network
+// The cache will never be used
+// This is the same as not using a service worker
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         fetch(event.request);
+//     );
+// });
+
+// Network with cache fallback
+// This cache strategy prefers the network
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(cacheResponse => cacheResponse ? cacheResponse :
-                fetch(event.request)
-                    .then(fetchResponse => caches.open(DYNAMIC_CACHE_KEY).then( // Dynamic cache fetch requests
-                        cache => {
+        fetch(event.request)
+            .then(fetchResponse => {
+                    return caches.open(DYNAMIC_CACHE_KEY)
+                        .then(cache => { // Dynamic cache fetch requests
                             cache.put(event.request.url, fetchResponse.clone()); // Clone because put consumes the response
                             return fetchResponse
-                        }))
-                    .catch(error =>{})
-            )
+                        })
+                })
+            .catch(error => {
+                return caches.match(event.request);
+            })
     );
 });
